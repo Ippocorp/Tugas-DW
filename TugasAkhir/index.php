4f3ade162779dfb0e3d96de75e752d9729c7f18b
Irfan26 <33430012+Irfan26@users.noreply.github.com>
2018-01-07 13:52:48 +0700

    Add files via upload
    Tugas Desain Web

---
+++ TugasAkhir/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Flat desain : Adobe CS6</title>
+        <link rel="stylesheet" type="text/css" href="assets/css/onepage-scroll.css" />
+        <link href='assets/fonts/Righteous.css' rel='stylesheet' type='text/css'>
+        <link href='assets/fonts/Voltaire.css' rel='stylesheet' type="text/css">
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head> 
+    <body>
+        <header>
+            <img src="assets/img/Adobe.jpg" alt="logo mini">
+            <h1><a href="#">Adobe Master Collection CS-6</a></h1>
+        </header>
+        <header>
+         	<nav>
+        		<ul>
+            		<a href="#"><li>Home</li></a>
+               		<a href="#"><li>Download</li></a>
+               	 	<a href="#"><li>Tutorial</li></a>
+                	<a href="#"><li>About Us</li></a>
+            	</ul>
+        	</nav>
+        </header>
+        <div class="main">
+            <section class="page one">
+                <div class="page-container">
+                    <h2><img src="assets/img/ps.jpg" alt="logo mini"> Adobe Photoshop CS6</h2>
+                    <p align="justify"> Adobe Photoshop adalah program aplikasi desain gambar, edit image grafis, dan mengolah foto digital. Versi terbaru program ini adalah Adobe Photoshop CS6 (Creative Suite 6) Beta, dirilis tanggal 21-3-2012. Software ini dibuat dan dikembangkan oleh Adobe Systems Incorporated, perusahaan terkemuka di bidang pengembangan perangkat lunak grafis dan multimedia. Perangkat lunak komputer ini memiliki kelebihan pada kelengkapan fitur desain grafis dan olah fotonya. Adobe Photoshop juga menyediakan fitur Filter yang bisa dipakai secara instan. Untuk pengguna tingkat lanjut, fitur seperti Styles dan Blending Options memberikan keleluasaan bagi desainer untuk berkreasi secara maksimal sesuai kreativitasnya.<br>Review
+</p>
+                    <video width="500" controls>
+                    <source src="assets/vid/Photoshop CS6 - Review.MP4" type="video/mp4"/> 
+                    
+                </div>
+            </section>
+            
+            <section class="page two">
+                <div class="page-container">
+                    <h2><img src="assets/img/pr.jpg" alt="logo mini"> Adobe Premiere pro cs6</h2>
+                    <p align="justify">Adobe Premiere Pro CS6 Full Version adalah sebuah aplikasi yang dapat mengedit video, audio, serta photo anda. Dengan menggunakan Adobe Premiere Pro CS6 Full Version hasil yang di dapat akan lebih memuaskan dan optimal. Adobe Systems Incorporated (Nasdaq:ADBE) mengumumkan perangkat terbaru untuk para profesional desain, web dan video dengan peluncuran lini produk Adobe® Creative Suite® 6. Rilis terbaru 14 aplikasi CS6 dan empat Creative Suites yang kuat – Adobe Creative Suite 6 Design & Web Premium, Adobe Creative Suite 6 Design Standard, Adobe Creative Suite 6 Production Premium; dan Adobe Creative Suite 6 Master Collection – menegaskan inovasi Adobe yang terus-menerus di semua pasar kreatif.<br>Review </p>
+                     <video width="500" controls>
+                    <source src="assets/vid/Adobe Premiere Pro CS6 Review.MP4" type="video/mp4"/> 
+                    
+                </div>
+            </section>
+            
+            <section class="page three">
+                <div class="page-container">
+                    <h2><img src="assets/img/dw.jpg" alt="logo mini"> Adobe Dreamweaver CS6</h2>
+                    <p align="justify">Adobe Dreamweaver CS6 adalah sebuah aplikasi yang digunakan untuk membuat website yang menggunakan HTML, PHP, Java script, dll. Bagi kalian yang ingin belajar Webdesign bisa menggunakan aplikasi ini. Adobe Dreamweaver ini memudahkan kalian dalam belajar membuat website. Biasanya Adobe dreamweaver juga sering digunakan oleh Web Developer/ orang yang membuat website.<br>Review </p>
+                     <video width="500" controls>
+                    <source src="assets/vid/Dreamweaver CC Review.MP4" type="video/mp4"/> 
+                    
+                </div>
+            </section>
+        </div>
+
+        <script type="text/javascript" src="assets/js/jquery-1.9.1.js"></script>
+        <script type="text/javascript" src="assets/js/jquery.onepage-scroll.js"></script>
+        <script type="text/javascript">
+        $(".main").onepage_scroll({
+                sectionContainer: "section",
+                easting: "cubic-bezier(0.180, 0.900, 0.410, 1.210)"
+        });
+        </script>
+    </body>
+</html>
